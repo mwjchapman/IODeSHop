33bfe256fc29cc47014556ae84b05131880e43ae
Mwjchapman <{23201760}+{mwjchapman}@users.noreply.github.com>
2023-07-30 21:23:17 +1200

    .

--- iodEshop.js
+++ iodEshop.js
@@ -5,10 +5,5 @@
         alert ("Your order has now shipped!")
         }
 
-        let seas = ['water sea', 'black sea', 'white sea', 'red sea', 'no sea'];
-        index = seas.indexOf('1');
-        if (index !== -1) {
-                arr[index] = 'snow sea';
-                console.log(seas);
-        }
+        
 
