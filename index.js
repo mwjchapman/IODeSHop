6b075100586b932c063f9fab400f09b36a56018b
Mwjchapman <{23201760}+{mwjchapman}@users.noreply.github.com>
2023-07-30 21:22:46 +1200

    committed

---
+++ index.js
@@ -0,0 +1,21 @@
+let mountains= ['Everest', 'Fuji', 'Nanga Parbat'];
+console.log(mountains[0]); 
+console.log(mountains[1]);
+console.log(mountains[2]);
+
+let seas= ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+console.log(seas);
+const firstElement = seas.shift();
+console.log(seas)
+seas.push("red sea");
+console.log(seas);
+
+const BOOK = {
+    "Title" : "The great book",
+    "Author" : "Me",
+    "Description" : "This book is about my coding journey",
+    "Pages" : 987
+}
+console.log(BOOK.Title);
+console.log(BOOK.Author);
+console.log(BOOK.Pages)
